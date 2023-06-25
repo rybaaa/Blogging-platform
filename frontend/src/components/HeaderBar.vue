3cdf9aeaba332d1b0bb6ad3bdfd57322d7478026
antonrybak <anton.rybak@internetbrands.com>
2023-06-25 21:23:00 +0200

    removing unnecessary files and changing existing

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "HeaderBar",
+  }
+</script>
+
+<template>
+    <h1>Test2</h1>
+</template>
+
+
+
+<style scoped lang="scss">
+@import '@/assets/sass/style.scss';
+</style>
+     
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "HomePage",
+  }
+</script>
+
+<template>
+    <h1>Test</h1>
+</template>
+
+
+
+<style scoped lang="scss">
+@import '@/assets/sass/style.scss';
+</style>
+     
