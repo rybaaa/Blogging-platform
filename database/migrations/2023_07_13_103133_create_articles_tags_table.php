9c95ba0edd55a492a20bdb0d2fd30a0c0f3ad8bc
antonrybak <anton.rybak@internetbrands.com>
2023-07-17 20:53:33 +0200

    fixed bug with migration table

--- database/migrations/2023_07_13_103133_create_articles_tags_table.php
+++ database/migrations/2023_07_13_103133_create_articles_tags_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('articles_tags');
+        Schema::dropIfExists('article_tag');
     }
 };
