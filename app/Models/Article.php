618561a3bd8d9d2f360839efdeee1957fab10bc7
antonrybak <anton.rybak@internetbrands.com>
2023-08-01 22:13:22 +0200

    storage articles covers

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,7 +14,7 @@ class Article extends Model
 
     public function comments()
     {
-        return $this-> hasMany(Comment::class);
+        return $this->hasMany(Comment::class);
     }
 
     public function author()
@@ -23,5 +24,12 @@ class Article extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function coverUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn (?string $coverUrl) => $coverUrl ? asset($coverUrl) : null
+        );
     }
 }
